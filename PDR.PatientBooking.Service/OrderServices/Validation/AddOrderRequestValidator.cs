2103759e65ff2d3be08b8e57e4b67c5a455e97a3
Andrey_Glazunov <andrey.glazunov@dataart.com>
2020-11-03 13:30:41 +0300

    Ticket-2 Added time-based validation to OrderCreateValidator

--- PDR.PatientBooking.Service/OrderServices/Validation/AddOrderRequestValidator.cs
+++ PDR.PatientBooking.Service/OrderServices/Validation/AddOrderRequestValidator.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using PDR.PatientBooking.Data;
 using PDR.PatientBooking.Service.OrderServices.Requests;
 using PDR.PatientBooking.Service.Validation;
@@ -18,13 +20,39 @@ namespace PDR.PatientBooking.Service.OrderServices.Validation
         {
             var result = new PdrValidationResult(true);
 
+            if (MissingRequiredFields(request, ref result))
+                return result;
+
             if (PatientNotFound(request, ref result))
                 return result;
 
             if (DoctorNotFound(request, ref result))
                 return result;
 
+            if (DoctorIsAlreadyScheduled(request, ref result))
+                return result;
+
             return result;
+        }
+
+        private bool MissingRequiredFields(AddOrderRequest request, ref PdrValidationResult result)
+        {
+            var errors = new List<string>();
+
+            if (request.StartTime > request.EndTime)
+                errors.Add("EndTime should be greater than StartTime");
+
+            if (request.StartTime < DateTime.UtcNow)
+                errors.Add("StartTime should be greater than current time");
+
+            if (errors.Any())
+            {
+                result.PassedValidation = false;
+                result.Errors.AddRange(errors);
+                return true;
+            }
+
+            return false;
         }
 
         private bool PatientNotFound(AddOrderRequest request, ref PdrValidationResult result)
@@ -45,6 +73,20 @@ namespace PDR.PatientBooking.Service.OrderServices.Validation
             {
                 result.PassedValidation = false;
                 result.Errors.Add("A doctor with that ID could not be found");
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool DoctorIsAlreadyScheduled(AddOrderRequest request, ref PdrValidationResult result)
+        {
+            if (_context.Order.Any(x => x.DoctorId == request.DoctorId
+                                        && !((request.StartTime < x.StartTime && request.EndTime < x.StartTime) ||
+                                             (request.StartTime > x.EndTime && request.EndTime > x.EndTime))))
+            {
+                result.PassedValidation = false;
+                result.Errors.Add("A doctor is already scheduled for this time");
                 return true;
             }
 
