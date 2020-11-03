bd9186ce67d120f1259bada3d97707a03e67eaf9
Andrey_Glazunov <andrey.glazunov@dataart.com>
2020-11-03 12:16:27 +0300

    Ticket-1 Fixed DoctorRequestValidator tests

---
+++ PDR.PatientBooking.Service/Extensions/StringExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+
+namespace PDR.PatientBooking.Service.Extensions
+{
+    public static class StringExtensions
+    {
+        private static Regex _emailRegex =
+            new Regex(@"^[^@\s]+@[^@\s]+\.[^@\s]+$", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+        
+        public static bool IsEmailAddress(this string sourceString)
+        {
+            if (string.IsNullOrEmpty(sourceString))
+            {
+                return false;
+            }
+
+            return _emailRegex.IsMatch(sourceString);
+        }
+    }
+}
