73ea1ee78e3673b09e685cdc43576591a78ea595
Andrey_Glazunov <andrey.glazunov@dataart.com>
2020-11-03 15:13:58 +0300

    Ticket-2 Fixed OrderRequestValidator tests

--- PDR.PatientBooking.Service/OrderServices/Requests/AddOrderRequest.cs
+++ PDR.PatientBooking.Service/OrderServices/Requests/AddOrderRequest.cs
@@ -4,7 +4,6 @@ namespace PDR.PatientBooking.Service.OrderServices.Requests
 {
     public class AddOrderRequest
     {
-        public Guid Id { get; set; }
         public DateTime StartTime { get; set; }
         public DateTime EndTime { get; set; }
         public long PatientId { get; set; }
